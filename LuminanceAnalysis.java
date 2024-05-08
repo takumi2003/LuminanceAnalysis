b31891edfefed0e6cadb6eb03933fd8a6baf3b3e
takumi2003 <takumi20030819@gmail.com>
2024-05-09 03:34:35 +0900

    feat: LuminanceAnalysis.javaの追加

---
+++ LuminanceAnalysis.java
@@ -0,0 +1,36 @@
+import java.awt.image.BufferedImage;
+public class LuminanceAnalysis {
+
+	private double Red;
+
+	private double Green;
+	private double Blue;
+
+	private double Value;
+
+	public LuminanceAnalysis(BufferedImage anImage, double brightness) {
+	public void LuminanceAnalysis(BufferedImage anImage, double brightness) {
+
+	}
+
+	protected void initialization() {
+
+	}
+
+	public BufferedImage GenerateImages() {
+		return null;
+	}
+
+	public double BrightnessCalculation(BufferedImage anImage) {
+		return 0;
+	}
+
+	public BufferedImage ImageWidthConversion(BufferedImage anImage) {
+		return null;
+	}
+
+	public BufferedImage loadImage(String path) {
+		return null;
+	}
+
+}
