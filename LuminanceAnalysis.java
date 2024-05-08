2eec2cc476c131a832ef5ae238b56aab7d30b938
takumi2003 <takumi20030819@gmail.com>
2024-05-08 21:38:55 +0900

    first commit

---
+++ LuminanceAnalysis.java
@@ -0,0 +1,39 @@
+import java.awt.image.BufferedImage;
+
+public class LuminanceAnalysis {
+
+	private color color;
+
+	private double Red;
+
+	private double Green;
+
+	private double Blue;
+
+	private double Value;
+
+	public void LuminanceAnalysis(BufferedImage anImage, double brightness) {
+
+	}
+
+	protected void initialization() {
+
+	}
+
+	public BufferedImage GenerateImages() {
+		return null;
+	}
+
+	public double BrightnessCalculation(BufferedImage anImage) {
+		return 0;
+	}
+
+	public BufferedImage ImageWidthConversion(BufferedImage anImage) {
+		return null;
+	}
+
+	public BufferedImage loadImage(String path) {
+		return null;
+	}
+
+}
